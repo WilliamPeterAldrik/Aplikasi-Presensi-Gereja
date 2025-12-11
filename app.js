db7b0d6569f9d69609e82bd2b8cf3945e7a9e548
Rachel837 <2372012@maranatha.ac.id>
2025-12-11 11:06:13 +0700

    memperbaiki tampilan dan menambahkan halaman web

--- app.js
+++ app.js
@@ -20,49 +20,61 @@ app.use(Myrouter);
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "home.html"));
+  res.sendFile(path.join(__dirname, "pages","ga_ada_akun", "home.html"));
 });
 
 // Route untuk setiap halaman
 app.get("/home.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "home.html"));
+  res.sendFile(path.join(__dirname, "pages","ga_ada_akun","home.html"));
 });
 
-app.get("/absen_u.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "absen_u.html"));
+app.get("/kegiatan.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages","ga_ada_akun","kegiatan.html"));
 });
 
 app.get("/aboutus_u.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "aboutus_u.html"));
+  res.sendFile(path.join(__dirname, "pages","ga_ada_akun", "aboutus_u.html"));
+});
+
+app.get("/akun_baru.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages","ga_ada_akun", "akun_baru.html"));
+});
+
+app.get("/absen_u.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "jemaat","absen_u.html"));
+});
+
+app.get("/home_admin.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "admin","home_admin.html"));
 });
 
 app.get("/contactus_u.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "contactus_u.html"));
+  res.sendFile(path.join(__dirname, "pages", "","contactus_u.html"));
 });
 
 app.get("/dashboard_admin.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "dashboard_admin.html"));
+  res.sendFile(path.join(__dirname, "pages", "admin","dashboard_admin.html"));
 });
 
 app.get("/lihat_users.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "lihat_users.html"));
+  res.sendFile(path.join(__dirname, "pages", "admin","lihat_users.html"));
 });
 
 app.get("/lihat_role.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "lihat_role.html"));
+  res.sendFile(path.join(__dirname, "pages", "admin","lihat_role.html"));
 });
 
 app.get("/add_role.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "add_role.html"));
+  res.sendFile(path.join(__dirname, "pages", "admin","add_role.html"));
 });
 
 app.get("/edit_role.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "edit_role.html"));
+  res.sendFile(path.join(__dirname, "pages","admin", "edit_role.html"));
 });
 
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "pages", "login.html"));
+  res.sendFile(path.join(__dirname, "pages","admin", "login.html"));
 });
 
 app.listen(port, () => console.log(`Running at http://localhost:${port}`));
